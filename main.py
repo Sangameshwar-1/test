81786165e7a4dce669168483e4fd5aa1f820af32
SALE SANGAMESHWAR <salesangameshwar1@gmail.com>
2025-01-17 23:43:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,16 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+def alert():
+    account_sid ="ACd0820c8fb546c38be1760536009548cc"
+    auth_token ="a07946d79b75d7a24937ba7ffcf5f083"
+    client = Client(account_sid, auth_token)
 
+    call = client.calls.create(
+                            twiml='<Response><Say>You are recieving this call because there has been a suspicious entry at the door please be alerted</Say></Response>',
+                            to='+918247341184',
+                            from_='+15856693126'
+                        )
 @app.get("/")
 def read_root():
     return "Welcome to FastAPI!"
@@ -22,6 +31,8 @@ def read_root():
 def say_hello_by_id(id: int):
     greetings = {1: "Sangameshwar", 2: "Rithik", 3: "Santhosh"}
     name = greetings.get(id, "Guest")
+    if id==1 :
+        alert()
     now = datetime.now()
     current_time = now.strftime("%H:%M:%S")
     if 5 <= now.hour < 12:
