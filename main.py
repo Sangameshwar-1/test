72d22358394701abbe234b60ed215c5c7c7d96a7
SALE SANGAMESHWAR <salesangameshwar1@gmail.com>
2025-01-17 23:46:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from datetime import datetime
+from twilio.rest import Client  # Ensure you have twilio installed
 
 app = FastAPI()
 
@@ -13,16 +13,19 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 def alert():
-    account_sid ="ACd0820c8fb546c38be1760536009548cc"
-    auth_token ="a07946d79b75d7a24937ba7ffcf5f083"
+    account_sid = "ACd0820c8fb546c38be1760536009548cc"
+    auth_token = "a07946d79b75d7a24937ba7ffcf5f083"
     client = Client(account_sid, auth_token)
 
     call = client.calls.create(
-                            twiml='<Response><Say>You are recieving this call because there has been a suspicious entry at the door please be alerted</Say></Response>',
-                            to='+918247341184',
-                            from_='+15856693126'
-                        )
+        twiml='<Response><Say>You are receiving this call because there has been a suspicious entry at the door please be alerted</Say></Response>',
+        to='+918247341184',
+        from_='+15856693126'
+    )
+    print("Alert sent")
+
 @app.get("/")
 def read_root():
     return "Welcome to FastAPI!"
@@ -31,7 +34,7 @@ def read_root():
 def say_hello_by_id(id: int):
     greetings = {1: "Sangameshwar", 2: "Rithik", 3: "Santhosh"}
     name = greetings.get(id, "Guest")
-    if id==1 :
+    if id == 1:
         alert()
     now = datetime.now()
     current_time = now.strftime("%H:%M:%S")
@@ -41,4 +44,4 @@ def say_hello_by_id(id: int):
         time_of_day = "afternoon"
     else:
         time_of_day = "evening"
-    return f"Hello, {name}!{time_of_day}"
+    return f"Hello, {name}! It is currently {current_time}, {time_of_day}."
