e0320e20778235e14ef3fd309928c754523ce1ae
SALE SANGAMESHWAR <salesangameshwar1@gmail.com>
2025-01-22 19:21:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import openai
@@ -23,20 +22,20 @@ def say_hello_by_id(id: int):
     greetings = {1: "Sangameshwar", 2: "Rithik", 3: "Santhosh"}
     name = greetings.get(id, "Guest")
     return f"Hello, {name}!"
+
 @app.get("/details/{id}")
-def say_datails(id: int):
-    details={1:["sangameshwar","sale"]}
-    deatail=details.get(id,"guest")
-    return deatail
+def say_details(id: int):
+    details = {1: ["sangameshwar", "sale"]}
+    detail = details.get(id, "guest")
+    return detail
+
 openai.api_key = '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'
-@app.get("/ask{ask}")
-def ask(pr):
+
+@app.get("/ask/{ask}")
+def ask(ask: str):
     response = openai.Completion.create(
-    engine="text-davinci-003",
-    prompt=pr,
-    max_tokens=5
+        engine="text-davinci-003",
+        prompt=ask,
+        max_tokens=5
     )
     return response.choices[0].text.strip()
-    
-    
-    
